2495f72f65097208ba4b288350866bffc14bf619
David <davidarutyunov77@gmail.com>
2024-11-18 13:45:26 +0100

    hw-11.2

--- main.js
+++ main.js
@@ -4,32 +4,52 @@
 
 // let secondLi = list[0];
 
-let changeColor = document.getElementsByTagName('li')
+// let changeColor = document.getElementsByTagName('li')
 
-let listCount = changeColor.length
+// let listCount = changeColor.length
 
-let secondLi = changeColor[1]
+// let secondLi = changeColor[1]
 
-secondLi.textContent = "thats a new text"
-
-
-console.log(secondLi)
+// secondLi.textContent = "thats a new text"
 
 
-let divColor = document.querySelector('.test')
-
-divColor.style.color = 'blue'
+// console.log(secondLi)
 
 
+// let divColor = document.querySelector('.test')
 
-const btnTest = document.getElementById('button')
-const inputField = document.querySelector('input')
+// divColor.style.color = 'blue'
 
 
-function pushToConsole() {
-  console.log(inputField.value)
+
+// const btnTest = document.getElementById('button')
+// const inputField = document.querySelector('input')
+
+
+// function pushToConsole() {
+//   console.log(inputField.value)
+// }
+
+// btnTest.addEventListener('click', pushToConsole)
+
+
+const divContainer = document.querySelector('.container')
+const textParagraph = document.querySelector('.text')
+const btnColor = document.getElementById('btn')
+
+textParagraph.style.background = 'blue'
+
+
+btnColor.addEventListener('click', changeColor);
+
+
+
+function changeColor() {
+  if (textParagraph.style.background ==='blue') {
+    textParagraph.style.background = 'red'
+  } else {
+    textParagraph.style.background = 'blue'
+  }
 }
-
-btnTest.addEventListener('click', pushToConsole)
 
 
