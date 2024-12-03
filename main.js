23dec2f85fe5a0be30009f4fef7300a1ea0e112b
David <davidarutyunov77@gmail.com>
2024-12-03 18:55:18 +0100

    hw-14.1

--- main.js
+++ main.js
@@ -33,3 +33,84 @@
 // btnTest.addEventListener('click', pushToConsole)
 
 
+const slider = document.getElementById('slider'); 
+const slides = document.getElementById('slides'); 
+const slide = document.getElementsByClassName('slide'); 
+const prevBtn = document.getElementById('prevBtn'); 
+const nextBtn = document.getElementById('nextBtn'); 
+const dots = document.getElementsByClassName('dot');
+
+
+
+
+let currentSlide = 0;
+
+
+function updateButtons() {
+  if (currentSlide === 0) {
+    prevBtn.style.display = 'none';
+  } else {
+    prevBtn.style.display = 'block';
+  }
+
+  if (currentSlide === slide.length - 1) {
+    nextBtn.style.display = 'none';
+  } else {
+    nextBtn.style.display = 'block';
+  }
+}
+
+
+function updateDots() {
+  for (let i = 0; i < dots.length; i++) {
+    dots[i].classList.remove('active');
+  }
+  dots[currentSlide].classList.add('active');
+}
+
+updateButtons()
+updateDots()
+
+
+
+
+
+
+prevBtn.addEventListener('click', (event) => {
+
+
+  
+    if (currentSlide > 0) {
+      slide[currentSlide].classList.remove('active')
+      currentSlide--
+      slide[currentSlide].classList.add('active')
+    } 
+    updateButtons()
+    updateDots()
+})
+
+
+
+
+nextBtn.addEventListener('click', (event) => {
+  if (currentSlide < slide.length - 1) {
+    slide[currentSlide].classList.remove('active')
+    currentSlide++
+    slide[currentSlide].classList.add('active')
+  } 
+  updateButtons()
+  updateDots()
+})
+
+
+
+  for (let i = 0; i < dots.length; i++) {
+    dots[i].addEventListener('click', () => {
+      slide[currentSlide].classList.remove('active')
+
+      currentSlide = i
+      slide[currentSlide].classList.add('active')
+      updateButtons()
+      updateDots()
+    })
+  }
